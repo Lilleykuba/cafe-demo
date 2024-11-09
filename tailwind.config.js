e5609cb4b2607a7e9580c6f5445c84f924d8d2a0
Camobeast <jakub.lilley@gmail.com>
2024-11-09 21:46:24 +0100

    bug fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,11 +3,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-      primary: '#a855f7',
-      secondary: '#ec4899',},
-  },
-  fontFamily: {
-    sans: ['Inter', 'sans-serif'],
+        primary: "#a855f7",
+        secondary: "#ec4899",
+      },
+    },
+    fontFamily: {
+      sans: ["Inter", "sans-serif"],
+    },
   },
   plugins: [require("daisyui")],
   daisyui: {
