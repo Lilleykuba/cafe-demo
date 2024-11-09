ad0529b3bb2a32f21bc7fce9c9e6fea69b4e0447
Camobeast <jakub.lilley@gmail.com>
2024-11-09 21:55:33 +0100

    minor changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { MenuIcon } from "@heroicons/react/outline";
 
 function Navbar() {
   return (
     <div className="navbar bg-base-100 shadow-md">
       <div className="container mx-auto">
         <div className="flex-1">
-          <MenuIcon className="h-6 w-6 text-gray-500" />
           <Link to="/" className="text-2xl font-bold">
             Cafe Demo
           </Link>
